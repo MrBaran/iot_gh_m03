8dbc4bdffc6e200ffc0dd4248ad9828f76b46bea
k2controls <kkelly@k2controls.net>
2019-01-26 13:52:39 -0500

    Update edevices_intro.py

--- edevices_intro.py
+++ edevices_intro.py
@@ -1,5 +1,6 @@
 from time import sleep
 from iot_gh.IoTGreenhouseService import IoTGreenhouseService
+from iot_gh.GHSwitches import GHSwitch
 
 ghs = IoTGreenhouseService()
 number = ghs.greenhouse.house_number
@@ -16,7 +17,7 @@ last_state = None
 while ghs.switches.push_button.is_off():
     state = ghs.switches.toggle.get_state()
     if state != last_state:
-        if state == ghs.switch.SWITCH_ON:
+        if state == GHSwitch.SWITCH_ON:
             ghs.lamps.red.on()
         else:
             ghs.lamps.red.off()
