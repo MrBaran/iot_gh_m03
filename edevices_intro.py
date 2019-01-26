d77fe1598c4b2c170400fb9f49c094780b0d6511
k2controls <kkelly@k2controls.net>
2019-01-26 13:49:46 -0500

    Update edevices_intro.py

--- edevices_intro.py
+++ edevices_intro.py
@@ -16,7 +16,7 @@ last_state = None
 while ghs.switches.push_button.is_off():
     state = ghs.switches.toggle.get_state()
     if state != last_state:
-        if state == ghs.switches.SWITCH_ON:
+        if state == ghs.switch.SWITCH_ON:
             ghs.lamps.red.on()
         else:
             ghs.lamps.red.off()
